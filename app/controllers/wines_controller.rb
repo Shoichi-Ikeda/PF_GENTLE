1f9f640ea76cec181192cabff7ca83cf4ae6cd4f
Shoichi-Ikeda <shoichann@gmail.com>
2021-04-14 09:42:02 +0000

    deploy

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -3,30 +3,31 @@ class WinesController < ApplicationController
     @wine = Wine.new
     @wines = Wine.all
   end
-  
+
   def show
     @wine = Wine.find(params[:id])
   end
-  
+
   def create
     @wine = Wine.new(wine_params)
     @wine.user_id = current_user.id
     @wine.save
+    redirect_to wines_path(@wine)
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   private
-  
+
   def wine_params
-    prams.require(:wine).permit(:wine_image_id, :wine_name, :type, :country, :variety, :vintage, :price, :rating)
+    params.require(:wine).permit(:wine_image, :wine_name, :kind, :country, :variety, :vintage, :price, :rating)
   end
-  
+
 end
---
+++ db/migrate/20210414073621_rename_type_column_to_wines.rb
@@ -0,0 +1,5 @@
+class RenameTypeColumnToWines < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :wines, :type, :kind
+  end
+end
