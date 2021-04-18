457ce726f07a6b9e21943e8f0f29bd70e4cc0b33
Shoichi-Ikeda <shoichann@gmail.com>
2021-04-18 10:23:04 +0000

    wine index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @wines = @user.wines
+    @wine = Wine.new
+  end
+
+  def index
+    @users = User.all
+    @wine = Wine.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   resources :wines, only: %i[index show create edit update destroy]
+
+  resources :users, only: [:index, :show, :edit, :update]
 end
