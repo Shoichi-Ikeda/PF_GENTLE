cba56a28dfcefc58be413a63f5fcbb2bf3cc186f
Shoichi-Ikeda <shoichann@gmail.com>
2021-04-20 03:45:06 +0000

    devise layout

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,24 +1,38 @@
 class WinesController < ApplicationController
   def index
     @wine = Wine.new
-    @wines = Wine.all
+    @wines_all = Wine.all
+    @wines = Wine.page(params[:page]).per(9)
   end
 
   def show
-    @wine = Wine.find(params[:id])
+    @wine = Wine.new
+    @wines = Wine.find(params[:id])
   end
 
   def create
-    @wine = Wine.new(wine_params)
+    wine_image = params[:wine]["wine_image"]
+    wine_name = params[:wine]["wine_name"]
+    kind = params[:wine]["kind"]
+    country = params[:wine]["country"]
+    variety = params[:wine]["variety"]
+    vintage = params[:wine]["vintage(1i)"]
+    price = params[:wine]["price"]
+    rating = params[:wine]["rating"]
+    @wine = Wine.new(wine_image: wine_image, wine_name: wine_name, kind: kind, country: country, variety: variety, vintage: vintage, price: price, rating: rating)
     @wine.user_id = current_user.id
     @wine.save
-    redirect_to wines_path(@wine)
+    redirect_to wines_path(@wine.id)
   end
 
   def edit
+    @wine = Wine.find(params[:id])
   end
 
   def update
+    @wine = Wine.find(params[:id])
+    @wine.update(wine_params)
+    redirect_to wines_path
   end
 
   def destroy
@@ -32,5 +46,4 @@ class WinesController < ApplicationController
   def wine_params
     params.require(:wine).permit(:wine_image, :wine_name, :kind, :country, :variety, :vintage, :price, :rating)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
-  resources :wines, only: %i[index show create edit update destroy]
+  resources :wines, only: [:index, :show, :create, :edit, :update, :destroy]
 
   resources :users, only: [:index, :show, :edit, :update]
 end
