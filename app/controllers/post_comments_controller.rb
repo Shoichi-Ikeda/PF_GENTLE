9613af55868560f451f3004a738f59235931b8e5
Shoichi-Ikeda <shoichann@gmail.com>
2021-05-03 04:43:14 +0000

    Google Natural Language　API追加

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 class PostCommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @wine = Wine.find(params[:wine_id])
     @post_comment = PostComment.new(post_comment_params)
     @post_comment.wine_id = @wine.id
     @post_comment.user_id = current_user.id
+    @post_comment.score = Language.get_data(post_comment_params[:comment])
     if @post_comment.save
       redirect_to request.referer
     else
