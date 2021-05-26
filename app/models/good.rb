65069ea8addab1f193ae0ebfd8e91da9b85df420
Shoichi-Ikeda <shoichann@gmail.com>
2021-05-26 12:08:15 +0000

    Goodモデル修正

--- app/models/good.rb
+++ app/models/good.rb
@@ -2,4 +2,8 @@
 class Good < ApplicationRecord
   attr_accessor :keyword
   validates :keyword, presence: true, length: { in: 2..20 }
+
+  def self.fetch_items_from_rakuten(keyword)
+    RakutenWebService::Ichiba::Item.search(keyword: keyword)
+  end
 end
