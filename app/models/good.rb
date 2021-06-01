8a5291b2099c0420da484c6c1cdee70c7515d051
Shoichi-Ikeda <shoichann@gmail.com>
2021-06-01 09:22:06 +0000

    Revert "Goodモデル修正"
    This reverts commit 65069ea8addab1f193ae0ebfd8e91da9b85df420.

--- app/models/good.rb
+++ app/models/good.rb
@@ -2,8 +2,4 @@
 class Good < ApplicationRecord
   attr_accessor :keyword
   validates :keyword, presence: true, length: { in: 2..20 }
-
-  def self.fetch_items_from_rakuten(keyword)
-    RakutenWebService::Ichiba::Item.search(keyword: keyword)
-  end
 end
